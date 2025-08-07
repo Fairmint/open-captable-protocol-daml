e6ab88007d0708491c0e5ea09481fd3c1f2b720a
HardlyDifficult <hardlydiff@gmail.com>
2025-08-07 14:12:46 -0400

    Publish dependencies

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,1 @@
-// Re-export everything from the generated library
 export * from '../generated/js/OpenCapTable-v01-0.0.1/lib';
-
-// Also export the package ID for convenience
-export { packageId } from '../generated/js/OpenCapTable-v01-0.0.1/lib'; 
