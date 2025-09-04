6fe0f3cb110e7652eb95decb3c3d1b9a5d140c35
HardlyDifficult <hardlydiff@gmail.com>
2025-09-04 12:10:59 -0400

    Bump version

--- scripts/create-package-index.ts
+++ scripts/create-package-index.ts
@@ -2,7 +2,7 @@ import fs from 'fs';
 import path from 'path';
 
 const packageDirs = [
-  path.join(__dirname, '..', 'generated', 'js', 'OpenCapTable-v15-0.0.1'),
+  path.join(__dirname, '..', 'generated', 'js', 'OpenCapTable-v16-0.0.1'),
   path.join(__dirname, '..', 'generated', 'js', 'OpenCapTableReports-v01-0.0.1'),
 ];
 
