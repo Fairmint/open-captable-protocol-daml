b13d463d7cc746bc008ed3c27d52aef44c3f6392
HardlyDifficult <hardlydiff@gmail.com>
2025-10-15 17:27:45 -0400

    Redeploy

--- scripts/create-package-index.ts
+++ scripts/create-package-index.ts
@@ -4,7 +4,7 @@ import path from 'path';
 const packageDirs = [
   path.join(__dirname, '..', 'generated', 'js', 'OpenCapTable-v25-0.0.1'),
   path.join(__dirname, '..', 'generated', 'js', 'OpenCapTableReports-v01-0.0.2'),
-  path.join(__dirname, '..', 'generated', 'js', 'Subscriptions-v08-0.0.1'),
+  path.join(__dirname, '..', 'generated', 'js', 'Subscriptions-v09-0.0.1'),
 ];
 
 // Create index.js and index.d.ts that re-export from lib/index.js if the directory exists
