2ac52f90addcf68b37f9ca93c68b4d7e2a9bed79
HardlyDifficult <hardlydiff@gmail.com>
2025-10-14 20:51:48 -0400

    Bump version

--- scripts/bundle-dependencies.ts
+++ scripts/bundle-dependencies.ts
@@ -7,7 +7,7 @@ import path from 'path';
 const PACKAGE_DIRS = [
 	path.join(__dirname, '../generated/js/OpenCapTable-v25-0.0.1'),
 	path.join(__dirname, '../generated/js/OpenCapTableReports-v01-0.0.2'),
-	path.join(__dirname, '../generated/js/Subscriptions-v02-0.0.1'),
+	path.join(__dirname, '../generated/js/Subscriptions-v03-0.0.1'),
 ];
 const DEPENDENCY_DIR = path.join(__dirname, '../generated/js/ghc-stdlib-DA-Internal-Template-1.0.0');
 const SPLICE_DEPENDENCY_DIR = path.join(__dirname, '../generated/js/splice-api-featured-app-v1-1.0.0');
@@ -549,7 +549,7 @@ function main(): void {
 			createBundledFiles(targetDir);
 			createBundledSpliceFiles(targetDir);
 			// Only bundle splice-amulet and DA Time Types for Subscriptions package
-			if (targetDir.includes('Subscriptions-v02')) {
+			if (targetDir.includes('Subscriptions-v03')) {
 				createBundledSpliceAmuletFiles(targetDir);
 				createBundledDATimeTypesFiles(targetDir);
 				createBundledDATypesFiles(targetDir);
